b478afb0fd96a1aec253afa3f650906a80185ad4
Bilusca <lucasav2005@hotmail.com>
2017-08-28 02:02:15 -0300

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const a = 2;
+function valA() {
+  return a;
+}
+valA();
---
+++ tests/main.spec.js
@@ -0,0 +1,48 @@
+var expect = require('chai').expect;
+
+describe('Main', function(){
+    var arr;
+    //roda uma vez, antes do bloco
+    before(function(){
+        
+    });
+
+    //roda uma vez, depois do bloco
+    after(function(){
+        
+    });
+
+    //roda uma vez, antes de CADA bloco
+    beforeEach(function(){
+        arr = [1,2,3];
+    });
+
+    //roda uma vez, depois de CADA bloco
+    afterEach(function(){
+        
+    });
+
+    //smoke teste para se testar se é algo ou se existe
+    it('should be an array', function(){
+        expect(arr).to.be.a('array');
+    });
+
+    it('should have a size of 4 when push another value to the array', function(){  
+        arr.push(4);
+        expect(arr).to.have.lengthOf(4);
+    });
+
+    it('should return true when pop 3 from the array', function(){
+        expect(arr.pop() === 3).to.be.true;
+    })
+
+    it('should have a size of 2 when pop a value from the array', function(){
+        arr.pop();
+        expect(arr).to.not.include(3);
+    });
+
+    it('should remove the value 3 when use pop in the array', function(){
+        arr.pop();
+        expect(arr).to.have.lengthOf(2);
+    });
+});
